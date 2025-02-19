559436ce22f168c6ccaa9278db0bd08b879f224f
Jay Wang <jianwangqau@gmail.com>
2025-02-18 23:24:08 -0800

    Add command line parameters

--- backend-koa/app.mjs
+++ backend-koa/app.mjs
@@ -1,8 +1,18 @@
 import Koa from 'koa';
+import { program } from 'commander';
+
+program
+    .option('-p, --port <port number>')
+    .parse();
+
+const options = program.opts();
+const port = options.port ? parseInt(options.port) : 3000;
+
 const app = new Koa();
 
 app.use(async ctx => {
-    ctx.body = 'Hello World';
+    ctx.body = 'Hello, koa server';
 });
 
-app.listen(3000);
+console.log('⇨ koa server listening on \x1b[32m' + port + '\x1b[0m');
+app.listen(port);
