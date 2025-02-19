d91400344b5f3fa34f5de8b3db2ad5cb42fac628
Jay Wang <jianwangqau@gmail.com>
2025-02-18 22:39:04 -0800

    Add command line parameters

--- backend-axum/src/main.rs
+++ backend-axum/src/main.rs
@@ -1,24 +1,30 @@
-//! Run with
-//!
-//! ```not_rust
-//! cargo run -p example-hello-world
-//! ```
+use structopt::StructOpt;
+use axum::{routing::get, Router};
 
-use axum::{response::Html, routing::get, Router};
+#[derive(StructOpt)]
+struct Cli {
+    #[structopt(short = "p", long = "port", default_value = "3000")]
+    port: u16,
+}
 
 #[tokio::main]
 async fn main() {
+    let args = Cli::from_args();
+    let port = &args.port;
+
     // build our application with a route
     let app = Router::new().route("/", get(handler));
 
     // run it
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+    let addr = format!("0.0.0.0:{}", port);
+    let listener = tokio::net::TcpListener::bind(addr)
         .await
         .unwrap();
-    println!("listening on {}", listener.local_addr().unwrap());
+
+    println!("⇨ axum server listening on \x1b[32m{}\x1b[0m", port);
     axum::serve(listener, app).await.unwrap();
 }
 
-async fn handler() -> Html<&'static str> {
-    Html("<h1>Hello, World!</h1>")
+async fn handler() -> &'static str {
+    "Hello, axum server"
 }
