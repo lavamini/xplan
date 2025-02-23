e0df349271252e0d7d469800d751fa064df7e567
Jay Wang <jianwangqau@gmail.com>
2025-02-23 02:28:26 -0800

    Add koa router

--- backend-koa/app.mjs
+++ backend-koa/app.mjs
@@ -1,4 +1,6 @@
 import Koa from 'koa';
+import Router from '@koa/router';
+import { bodyParser } from '@koa/bodyparser';
 import { program } from 'commander';
 
 program
@@ -9,10 +11,19 @@ const options = program.opts();
 const port = options.port ? parseInt(options.port) : 3000;
 
 const app = new Koa();
+const router = new Router();
 
-app.use(async ctx => {
-    ctx.body = 'Hello, koa server';
+// Parse request.body
+app.use(bodyParser());
+
+// Set handle /
+router.get('/', async (ctx, next) => {
+    ctx.response.type = 'text/plain';
+    ctx.response.body = 'Hello, koa server';
 });
+
+// Use router
+app.use(router.routes());
 
 console.log('⇨ koa server listening on \x1b[32m' + port + '\x1b[0m');
 app.listen(port);
