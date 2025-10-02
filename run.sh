30a2b8a3d402f6e146986ba22d482dc75d22fc59
Jay Wang <jianwangqau@gmail.com>
2025-10-02 14:54:12 -0700

    优化 run.sh

--- run.sh
+++ run.sh
@@ -3,18 +3,20 @@
 if [ $# -eq 0 ]; then
     printf "Usage: ./run.sh <axum | fiber | koa>\n"
 else
-    if [ "$1" == "axum" ]; then
+    target=$1
+
+    if [ $target == "axum" ]; then
         cd backend-axum
         cargo build --release
         cp ./target/release/backend-axum ./main
         RUST_LOG=info ./main
         cd ..
-    elif [ "$1" == "fiber" ]; then
+    elif [ $target == "fiber" ]; then
         cd backend-fiber
         go build -ldflags '-w -s' main.go router.go
         ./main
         cd ..
-    elif [ "$1" == "koa" ]; then
+    elif [ $target == "koa" ]; then
         cd backend-koa
         node app.js
         cd ..
